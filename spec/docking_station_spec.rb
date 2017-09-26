e8d5fc6e76dd28e9ec5e390968e8927124b3a14c
Chris <cdunham1989@gmail.com>
2017-09-26 17:59:38 +0100

    Step 10, check if new bike is working

--- spec/docking_station_spec.rb
+++ spec/docking_station_spec.rb
@@ -7,5 +7,12 @@ require "docking_station.rb"
 # end
 
 describe DockingStation do
+
   it { is_expected.to respond_to('release_bike') }
+
+  it "releases working bike" do
+    bike = subject.release_bike
+    expect(bike).to be_working
+  end
+
 end
