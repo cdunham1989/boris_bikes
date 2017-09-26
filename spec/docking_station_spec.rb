cb69596fe6d79cae4aff95fd8f8409c552bdaa60
Chris <cdunham1989@gmail.com>
2017-09-26 16:39:24 +0100

    step 8, single line rspec and method defined

--- spec/docking_station_spec.rb
+++ spec/docking_station_spec.rb
@@ -1,5 +1,11 @@
-require "dockingstation.rb"
+require "docking_station.rb"
+
+# describe DockingStation do
+#   it "responds to release bike" do
+#     expect(subject).to respond_to :release_bike
+#   end
+# end
 
 describe DockingStation do
-
+  it { is_expected.to respond_to('release_bike') }
 end
