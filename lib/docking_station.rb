3585822ecc331c91b95eb0b3ab8c3143690f66e5
Chris <cdunham1989@gmail.com>
2017-09-28 17:33:54 +0100

    Step 12, if dock is empty it raises error when attempting to release bike

--- lib/docking_station.rb
+++ lib/docking_station.rb
@@ -5,7 +5,8 @@ class DockingStation
   attr_reader :bike
 
   def release_bike
-    Bike.new
+    fail 'No bikes!' unless @bike
+    @bike
   end
 
   def dock(bike)
--- spec/docking_station_spec.rb
+++ spec/docking_station_spec.rb
@@ -7,9 +7,23 @@ describe DockingStation do
   it { is_expected.to respond_to('release_bike') }
 
   it "releases working bike" do
-     bike = subject.release_bike
+     bike = Bike.new
+     subject.dock(bike)
      expect(bike).to be_working
   end
+
+  describe '#release_bike' do
+    it 'releases a bike' do
+      bike = Bike.new
+      docking_station.dock(bike)
+      expect(docking_station.release_bike).to eq bike
+    end
+
+    it 'raises an error when there are no bikes available' do
+    expect { docking_station.release_bike }.to raise_error('No bikes!')
+    end
+  end
+
 
   it "docks a bike" do
     bike = Bike.new
